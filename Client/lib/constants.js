ffa9ad6365339b1b9beaa1eaa371ed544c229bd3
SahilPachlore <71082807+SahilPachlore@users.noreply.github.com>
2022-06-07 23:15:50 -0700

    Update constants.js

--- Client/lib/constants.js
+++ Client/lib/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json'
 
 export const contractABI = abi.abi
-export const contractAddress = '0x21FD9c127F71c12AD8a04E6aa05D4e2A99474E72'
+export const contractAddress = 'contract_address'
