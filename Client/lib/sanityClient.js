65d4eddcd5ea816bac93c8ae53abe8d1ceaaa079
SahilPachlore <71082807+SahilPachlore@users.noreply.github.com>
2022-06-07 23:16:21 -0700

    Update sanityClient.js

--- Client/lib/sanityClient.js
+++ Client/lib/sanityClient.js
@@ -5,6 +5,6 @@ export const client = sanityClient({
   dataset: 'production',
   apiVersion: 'v1',
   token:
-    'skljy3PAOEbf5cMCeCedHeTZgEMwMTSvz8Mr1fTyR1ULzlNPfdRzeo1DcEKY1ItcEld3xx3PTW0VHuSdYj2nFpMSgn12FLDB6uTbjFwARVwmvh6HQpInzbTMD3VWCyKt0Hroyk28ZARbkbhyoq9LLNraSGIAG6lpOeuCc8iPyoOlhUAevojD',
+    'token_id',
   useCdn: false,
 })
